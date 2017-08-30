b96e2e28def0f744d98c849bb1a702d103bec809
Liam Cain <cain529@gmail.com>
2017-08-30 17:34:24 -0400

    Remove extraneous templates from the provider

--- src/widgets/RadioBuilder.js
+++ src/widgets/RadioBuilder.js
@@ -1,4 +1,20 @@
+import invariant from 'invariant';
+
+import BaseBuilder from '../BaseBuilder';
 import SelectBuilder from '../primitives/SelectBuilder';
 
-export default SelectBuilder
+class RadioBuilder extends BaseBuilder {
+  /**
+   * Set the layout of the radio group to either 'horizontal' or 'vertical.'
+   * The template should use this to configure how to layout the buttons.
+   *
+   * @param {string} layout
+   */
+  setLayout(layout) {
+    invariant(layout === 'horizontal' || layout === 'vertical', 'Layout type is not allowed');
+    return this.setConfig({ layout });
+  }
+}
+
+export default new RadioBuilder()
   .setLazyTemplateFactory(provider => provider.getRadio());
